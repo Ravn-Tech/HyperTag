ed0b5c27d126420e0e8fae584d0d41e4c8085dfd
Sean <sean-p-96@hotmail.de>
2021-01-28 20:24:19 +0100

    Fix test_is_int

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ from hypertag import utils
 
 
 def test_is_int():
-    assert utils.is_int("42") == True
-    assert utils.is_int("3.4") == False
-    assert utils.is_int("ABC") == False
-    assert utils.is_int(3.141) == False
+    assert utils.is_int("42") is True
+    assert utils.is_int("3.4") is False
+    assert utils.is_int("ABC") is False
+    assert utils.is_int(3.141) is False
