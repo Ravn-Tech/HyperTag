2c949688d25e0d77cb89dca38c694dbec1f384c2
Sean <sean-p-96@hotmail.de>
2021-01-28 21:51:23 +0100

    Add case

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@ from hypertag import utils
 
 
 def test_is_int():
+    assert utils.is_int(42) is True
     assert utils.is_int("42") is True
     assert utils.is_int("3.4") is False
     assert utils.is_int("ABC") is False
