bef1e7e3e30d340baf7aa08cf3486d71d0a85d95
Sean Pedersen (CEO - Ravn.Tech) <sean.pedersen(AT)gmail.com>
2022-10-23 13:09:20 +0200

    Remove debug print

--- hypertag/hypertag.py
+++ hypertag/hypertag.py
@@ -420,7 +420,6 @@ class HyperTag:
         minus (difference)
         """
         # TODO: Parse AST to support queries with brackets
-        print("PARAQ", query)
         operands = {"and", "or", "minus"}
         tag_val = query[0].split("=")
         if len(tag_val) == 1:
